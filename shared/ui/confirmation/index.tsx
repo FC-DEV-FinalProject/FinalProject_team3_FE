02933e992f825a87911b4200e2e5a57afae88660
nanafromjeju <nanafromjeju@gmail.com>
2024-12-03 17:12:00 +0900

    refactor: Confirmation 컴포넌트 코드 수정 (#127)

--- shared/ui/confirmation/index.tsx
+++ shared/ui/confirmation/index.tsx
@@ -1,22 +1,34 @@
 import classNames from 'classnames/bind'
 
+import { Button } from '../button'
 import styles from './styles.module.scss'
 
 const cx = classNames.bind(styles)
 
 interface Props {
-  title: string
-  message: string
-  children?: string
+  onClick?: () => void
 }
 
-export const Confirmation = ({ title, message, children }: Props) => {
+export const Confirmation = ({ onClick }: Props) => {
   return (
     <div className={cx('container')}>
-      <div className={cx('title')}>{title}</div>
+      <p className={cx('title')}>회원탈퇴</p>
       <div className={cx('line')}></div>
-      <p className={cx('message')}>{message}</p>
-      {children}
+      <p className={cx('message')}>회원 탈퇴 메세지</p>
+      <div className={cx('content')}>
+        <div className={cx('message-wrapper')}>
+          <p>ㆍ탈퇴 즉시 모든 개인정보가 삭제됩니다.</p>
+          <p>ㆍ구독한 전략에 대한 모든 내용이 삭제됩니다.</p>
+        </div>
+      </div>
+      <div className={cx('button-wrapper')}>
+        <Button className={cx('left-button')} onClick={onClick}>
+          취소
+        </Button>
+        <Button className={cx('right-button')} variant="filled" onClick={onClick}>
+          탈퇴
+        </Button>
+      </div>
     </div>
   )
 }
